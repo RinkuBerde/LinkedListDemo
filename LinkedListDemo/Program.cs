185dfb1ad307121567e89d0bfb937a190b7dc654
rinkuberde <rinkuberde@gmail.com>
2023-01-18 20:16:10 +0530

    [Rinku] - linked list oprations

--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list demo program!!!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 51);
+            list.RemoveFirstNode();
+            list.Search(51);
+            list.RemoveLastNode();
+            list.Display();
         }
     }
 }
